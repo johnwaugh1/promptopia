9da477d79c61542379d1d60d1469bf78be7df68b
johnwaugh1 <waughjm@appstate.edu>
2024-10-20 21:40:19 -0400

    fixed auth and database issues

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,47 +1,45 @@
-import NextAuth from "next-auth";
+import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 import User from '@models/user';
-import { connectToDB } from "@utils/database";
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
+  callbacks: {
     async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
 
-        session.user.id = sessionUser._id.toString();
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-        return session;
+      return session
     },
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
-
-            const userExists = await User.findOne({
-                email: profile.email
-            });
-
-            if (!userExists) {
-                await User.Create({
-                    email: profile.email,
-                    username: profile.name.replace(' ', '').toLowerCase(),
-                    image: profile.picture
-                })
-            }
-
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await connectToDB();
+        const userExists = await User.findOne({ email: profile?.email });
+        // if not, create a new document and save user in MongoDB
+        if (userExists === null) {
+          await User.create({
+            email: profile?.email,
+            username: profile?.name.replace(/\s+/g, '').toLowerCase(),
+            image: user.image
+          })
         }
-    }
+
+        return true
+      } catch (error) {
+        console.log(error);
+        return false
+      }
+      
+    } 
+  }
 })
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
